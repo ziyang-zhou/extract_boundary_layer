74ff9ef985c0a4633769c2db66b703dd55304df2
ziyang-zhou <zhouziyang5@gmail.com>
2024-08-31 10:57:03 -0400

    indexing fix

--- read_boundary_layer/functions/analysis.py
+++ read_boundary_layer/functions/analysis.py
@@ -246,7 +246,7 @@ def get_length_scale(pfluc,x,y,x0,y0,x1,y1,threshold = 0.05,axis = 'column'):
 			loc_array = []
 			#Recompute the cross correlation array
 			for j,x_i in enumerate(x[mask_integrate_range][ki0:]): #moving point
-				p1 = pfluc[:,mask_plot_range,mask_integrate_range][:,i,j]
+				p1 = pfluc[:,mask_plot_range,:][:,i,ki0+j]
 				p0 = pfluc[:,mask_plot_range,:][:,i,ki0]
 				c = get_velocity_corr(p0,p1,y0_i,y0_i)
 				if c > threshold:
