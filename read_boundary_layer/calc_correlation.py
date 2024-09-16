aad2de3f1fbaa69e8794534a83d430f14eacb905
ziyang-zhou <zhouziyang5@gmail.com>
2024-09-15 20:44:36 -0400

    restored

--- read_boundary_layer/calc_correlation.py
+++ read_boundary_layer/calc_correlation.py
@@ -56,30 +56,26 @@ xcoor0 = temporal.xcoor0 # x location of the integration axis
 r=Reader('hdf_antares')
 r['filename'] = mesh_read_path + 'interpolation_3d_grid.h5'
 BL_line_geom=r.read()
-xcoor = BL_line_geom[0][0]['x'][:,0,0]
-ycoor = BL_line_geom[0][0]['y'][:,0,0] 
-ds = np.sqrt((xcoor[1:]-xcoor[:-1])**2 + (ycoor[1:]-ycoor[:-1])**2)
-sprof = np.zeros(ds.size+1,)
-sprof[1:] = np.cumsum(ds)
-scoor = sprof
 print('shape of BL line geom',BL_line_geom[0][0]['x'].shape)
 
 #For every timestep form a 2D matrix of velocities (wall normal , streamwise)
 #Array dim : 1 is time, 2 is wall normal and 3 is chordwise
-pfluc_path = bl_read_path + 'pfluc.npy'
-
 for j in range(num_chunks):
 	#Read the boundary layer history at x_loc
 	r = Reader('hdf_antares')
 	r['filename'] = bl_read_path + 'BL_line_prof_{}_{}.h5'.format(starting_timestep+j*step_per_chunk,starting_timestep+(j+1)*(step_per_chunk))
 	BL_line_prof = r.read()
-
+  
 	if j == 0:
 		#creation of streamwise distance array
+		xcoor = BL_line_geom[0][0]['x'][:,0,0]
+		ycoor = BL_line_geom[0][0]['y'][:,0,0] 
+		ds = np.sqrt((xcoor[1:]-xcoor[:-1])**2 + (ycoor[1:]-ycoor[:-1])**2)
+		sprof = np.zeros(ds.size+1,)
+		sprof[1:] = np.cumsum(ds)
+		scoor = sprof
+
 		hcoor = BL_line_prof[0][0]['h'][0,:] # Read the wall normal distance 
-		if pfluc_path.is_file():
-			data = np.load(pfluc_path)
-			break
 
 	for n,i in enumerate(BL_line_prof[0].keys()[1:]):
 		for m in range(len(xcoor)):  # read all spatial locations in the current timestep
@@ -101,7 +97,6 @@ for j in range(num_chunks):
 
 	print('data shape is {}'.format(np.shape(data)))
 	print('chunk {} read'.format(j))
-np.save(pfluc_path, data)
 
 
 # ------------------------------
@@ -112,8 +107,9 @@ nbi = data.shape[0]
 meandata = data.mean(axis=0,dtype=np.float64)
 
 #Setting the fixed point
-l0 = analysis.find_nearest(hcoor,temporal.h_0_bar*delta_95) #wall normal coordinate of fixed point
+l0 = analysis.find_nearest(hcoor,temporal.h_0*delta_95) #wall normal coordinate of fixed point
 ki0 = analysis.find_nearest(xcoor,xcoor0) #streamwise coordinate of fixed point
+h_mask_delta_95 = (hcoor < delta_95) #Mask to scope out the boundary layer
 
 #Compute the arithmetic mean along the specified axis.
 pfluc = data - np.tile(meandata,(nbi,1,1))
