5fd900192ae0924508c496cbc9c96ad7c21e11a0
ziyang-zhou <zhouziyang5@gmail.com>
2024-11-10 10:06:09 -0500

    pickle loading of data_dict added

--- read_boundary_layer/03-turbulence_calc.py
+++ read_boundary_layer/03-turbulence_calc.py
@@ -6,6 +6,7 @@ import vtk
 import matplotlib.pyplot as plt
 import numpy as np
 import temporal
+import pickle
 import os
 import math
 import h5py
@@ -83,62 +84,69 @@ coordinates_df.to_csv(bl_save_path + '{}_coordinates.csv'.format(case_name))
 
 print('Loading data...')
 data_dict = {}
-for var in var_list:
-	#For every timestep form a 2D matrix of velocities (wall normal , streamwise)
-	#Array dim : 1 is time, 2 is wall normal and 3 is chordwise
-	for j in range(num_chunks):
-		#Read the boundary layer history at x_loc
-		r = Reader('hdf_antares')
-		r['filename'] = bl_read_path + 'BL_line_prof_{}_{}.h5'.format(starting_timestep+j*step_per_chunk,starting_timestep+(j+1)*(step_per_chunk))
-		BL_line_prof = r.read()
-	
-		if j == 0:
-			#creation of streamwise distance array
-			xcoor = BL_line_geom[0][0]['x'][:,0,0]
-			ycoor = BL_line_geom[0][0]['y'][:,0,0] 
-			ds = np.sqrt((xcoor[1:]-xcoor[:-1])**2 + (ycoor[1:]-ycoor[:-1])**2)
-			sprof = np.zeros(ds.size+1,)
-			sprof[1:] = np.cumsum(ds)
-			scoor = sprof
+if os.path.isfile(bl_save_path + 'data_dict.pkl'):
+	data_dict = pickle.load(bl_save_path + 'data_dict.pkl')
+	print(bl_save_path + 'data_dict.pkl','already exists')
+else:
+	for var in var_list:
+		#For every timestep form a 2D matrix of velocities (wall normal , streamwise)
+		#Array dim : 1 is time, 2 is wall normal and 3 is chordwise
+		for j in range(num_chunks):
+			#Read the boundary layer history at x_loc
+			r = Reader('hdf_antares')
+			r['filename'] = bl_read_path + 'BL_line_prof_{}_{}.h5'.format(starting_timestep+j*step_per_chunk,starting_timestep+(j+1)*(step_per_chunk))
+			BL_line_prof = r.read()
+		
+			if j == 0:
+				#creation of streamwise distance array
+				xcoor = BL_line_geom[0][0]['x'][:,0,0]
+				ycoor = BL_line_geom[0][0]['y'][:,0,0] 
+				ds = np.sqrt((xcoor[1:]-xcoor[:-1])**2 + (ycoor[1:]-ycoor[:-1])**2)
+				sprof = np.zeros(ds.size+1,)
+				sprof[1:] = np.cumsum(ds)
+				scoor = sprof
 
-			hcoor = BL_line_prof[0][0]['h'][0,:] # Read the wall normal distance
+				hcoor = BL_line_prof[0][0]['h'][0,:] # Read the wall normal distance
 
-		for n,i in enumerate(BL_line_prof[0].keys()[1:]):
-			for m in range(len(xcoor)):  # read all spatial locations in the current timestep
-				profile_append = np.array(BL_line_prof[0][i][var][m])
-				if (m==0):
-					profile = profile_append #Create the data to be appended by concatenating the wall normal profiles for each x location
-				elif (m==1):
-					profile = np.concatenate((profile[:,np.newaxis], profile_append[:,np.newaxis]), axis=1)
-				else :
-					profile = np.concatenate((profile, profile_append[:,np.newaxis]), axis=1)
+			for n,i in enumerate(BL_line_prof[0].keys()[1:]):
+				for m in range(len(xcoor)):  # read all spatial locations in the current timestep
+					profile_append = np.array(BL_line_prof[0][i][var][m])
+					if (m==0):
+						profile = profile_append #Create the data to be appended by concatenating the wall normal profiles for each x location
+					elif (m==1):
+						profile = np.concatenate((profile[:,np.newaxis], profile_append[:,np.newaxis]), axis=1)
+					else :
+						profile = np.concatenate((profile, profile_append[:,np.newaxis]), axis=1)
 
-			data_append = profile
-			if (n == 0) and (j == 0):
-				data = data_append
-			elif (n == 1) and (j == 0):
-				data = np.concatenate((data[np.newaxis,:,:], data_append[np.newaxis,:,:]), axis=0)
-			else:
-				data = np.concatenate((data, data_append[np.newaxis,:,:]), axis=0)
+				data_append = profile
+				if (n == 0) and (j == 0):
+					data = data_append
+				elif (n == 1) and (j == 0):
+					data = np.concatenate((data[np.newaxis,:,:], data_append[np.newaxis,:,:]), axis=0)
+				else:
+					data = np.concatenate((data, data_append[np.newaxis,:,:]), axis=0)
 
-		print('data shape is {}'.format(np.shape(data)))
-		print('chunk {} read'.format(j))
+			print('data shape is {}'.format(np.shape(data)))
+			print('chunk {} read'.format(j))
 
-	if if_interpolate == True:
-		for ki in range(0,np.shape(data)[2]-1):
-			for t in range(0,np.shape(data)[0]-1):
-				i_zero = np.where(abs(data[t,:,ki]) == 0)[0]
-				for i in i_zero:
-					if i+1 not in i_zero:
-						data[t,i,ki] = (data[t,i-1,ki] + data[t,i+1,ki])/2
-					else:
-						#Obtain the next non-zero value for interpolation
-						n=0
-						while (data[t,i+n,ki]==0):
-							n+=1
-						data[t,i,ki] = (data[t,i-1,ki] + data[t,i+n,ki])/2
+		if if_interpolate == True:
+			for ki in range(0,np.shape(data)[2]-1):
+				for t in range(0,np.shape(data)[0]-1):
+					i_zero = np.where(abs(data[t,:,ki]) == 0)[0]
+					for i in i_zero:
+						if i+1 not in i_zero:
+							data[t,i,ki] = (data[t,i-1,ki] + data[t,i+1,ki])/2
+						else:
+							#Obtain the next non-zero value for interpolation
+							n=0
+							while (data[t,i+n,ki]==0):
+								n+=1
+							data[t,i,ki] = (data[t,i-1,ki] + data[t,i+n,ki])/2
 
-	data_dict[var] = data
+		data_dict[var] = data
+
+	with open(bl_save_path + 'data_dict.pkl', 'wb') as f:
+		pickle.dump(data_dict, f)
 # ------------------------------
 # Parameter calculation
 # ------------------------------
